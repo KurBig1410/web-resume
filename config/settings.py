9ef098aff68b62a2dc283ce342d4e70233890471
Suhrab <suhrab.k@mail.ru>
2025-06-24 00:57:17 +0700

    Настройка конфигурации. Перенос данных в переменную окружения

--- config/settings.py
+++ config/settings.py
@@ -1,16 +1,19 @@
 from pathlib import Path
+from decouple import config
+from dj_database_url import parse as db_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@edz2gbkyf5v%q3t)7zfsil**46!6b7_#8jse$2d&j3b7u*#qv"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
